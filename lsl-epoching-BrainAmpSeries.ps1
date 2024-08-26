8b3b90a72af231616282aa0f4d6a46c4346ba8eb
Simon Kojima <simon.kojima@outlook.com>
2024-08-26 16:08:28 +0900

    Update lsl-epoching-BrainAmpSeries.ps1

--- lsl-epoching-BrainAmpSeries.ps1
+++ lsl-epoching-BrainAmpSeries.ps1
@@ -2,5 +2,5 @@ conda activate asme
 python ./main.py --marker scab-c `
                  --signal BrainAmpSeries `
                  --port 49153 `
-                 --log  C:\Users\simon\Documents\eeg\asme-speller\sub-tech\ses-S001\log
+                 --log  C:\Users\simon\Documents\eeg\asme-speller\sub-C\ses-S001\log
 pause
